da91b5589afbce748bc065e9efa780b29ecdf08d
maxime <paxman2@hotmail.fr>
2015-03-10 21:41:19 +0100

    Initial commit

---
+++ js/mainController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by maximeb on 10/03/15.
+ */
+
+
+angular.module("mainApp", []);
+
+angular.module("mainApp").controller("mainController", [ "$scope", function() {
+
+    controller("MainController", ["$scope"]);
+
+}]);
